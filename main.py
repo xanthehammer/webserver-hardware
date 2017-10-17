83e04d1a701ff51be488e943582138a336a9441d
Oxxyg33n <alexand3r.logvinenko@gmail.com>
2017-10-17 12:36:03 +0300

    Initialise repository

---
+++ main.py
@@ -0,0 +1,51 @@
+import socket
+#from machine import Pin
+#led_pin = Pin(5, Pin.OUT)
+
+CONTENT = """\
+HTTP/1.0 200 OK
+Content-Type: text/html
+
+<html>
+  <head>
+  </head>
+  <body>
+    <p>Hello #%d from MicroPython!</p>
+    <a href="/toggle">Click here to toggle LED hooked to pin 5</a>
+  </body>
+</html>
+"""
+
+def main():
+    s = socket.socket()
+    ai = socket.getaddrinfo("0.0.0.0", 8080)
+    print("Bind address info:", ai)
+    addr = ai[0][-1]
+
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(addr)
+    s.listen(5)
+    print("Listening, connect your browser to http://localhost:8080/")
+
+    counter = 0
+    while True:
+        sock, addr = s.accept()
+        print("Client address:", addr)
+        stream = sock.makefile("rwb")
+        req = stream.readline().decode("ascii")
+        method, path, protocol = req.split(" ")
+        print("Got", method, "request for")
+        #if path == "/toggle":
+            #led_pin.value(1-led_pin.value())
+        while True:
+            h = stream.readline().decode("ascii").strip()
+            if h == "":
+                break
+            print("Got HTTP header:", h)
+        stream.write(CONTENT % counter)
+        stream.close()
+        sock.close()
+        counter += 1
+        print()
+
+main() # Press Ctrl-C to stop web server
